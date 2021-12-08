b1becd5bd873e5bf3a367ad9098dd4ebe716ad81
Billy Rebecchi <billy.rebecchi@googlemail.com>
2021-12-08 20:52:14 +0000

    Setup registers and improve editor

--- editor.js
+++ editor.js
@@ -1,6 +1,20 @@
-function loadAutomaton(automatron) {
-    document.getElementById("iRegister").value = automatron.iRegister;
-    document.getElementById("oRegister").value = automatron.oRegister;
-    document.getElementById("fRegister").value = automatron.fRegister;
-    document.getElementById("code").value = automatron.instructions;
+function loadLineNumbers() {
+    var nums = "";
+    for (var i = 1; i <= 256; i++) {
+        nums += `${i}\n`;
+    }
+    document.getElementById("instructions-editor").value = nums;
+    document.getElementById("ram-editor").value = nums;
+}
+
+function loadAutomaton(automaton) {
+    document.getElementById("a").value = automaton.registers["a"];
+    document.getElementById("b").value = automaton.registers["b"];
+    document.getElementById("c").value = automaton.registers["c"];
+    document.getElementById("f").value = automaton.registers["f"];
+    document.getElementById("hl").value = automaton.registers["hl"];
+    document.getElementById("pc").value = automaton.registers["pc"];
+    document.getElementById("opcode").value = automaton.registers["opcode"];
+    document.getElementById("operand").value = automaton.registers["operand"];
+    document.getElementById("code").value = automaton.instructions;
 }
--- objects.js
+++ objects.js
@@ -10,9 +10,16 @@ class Automaton {
         this.x = x;
         this.y = y;
 
-        this.iRegister = "0000";
-        this.oRegister = "0000";
-        this.fRegister = "0000";
+        this.registers = {
+            "a": "00000000",
+            "b": "00000000",
+            "c": "00000000",
+            "f": "00000000",
+            "pc": "00000000",
+            "hl": "00000000",
+            "opcode": "0000",
+            "operand": "00000000"
+        };
 
         this.instructions = "";
     }
